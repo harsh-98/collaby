f311f4583a996a8ee33c8fb1897f336230b68969
harsh_jain <harshjniitr@gmail.com>
2018-05-28 02:12:03 +0530

    improving the architecture

---
+++ src/apps/index.js
@@ -0,0 +1,18 @@
+import bookmarks from './BookmarksApplication';
+import kpt from './KPTApplication';
+import datematcher from './DateMatcherApplication';
+import meetingtime from './MeetingTimeApplication';
+import vote from './VoteApplication';
+import kanban from './KanbanApplication';
+import youtube from './YoutubeApplication';
+
+export default {
+  bookmarks,
+  kpt,
+  datematcher,
+  error,
+  meetingtime,
+  vote,
+  kanban,
+  youtube,
+};
